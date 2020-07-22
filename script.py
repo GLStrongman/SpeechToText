438731bead8fcbcf3dfd5ef7348a3d37dc0d89a8
GStrongman <georgia.strongman@gmail.com>
2020-07-22 19:07:48 +1200

    Add documentation, file write function
    Added comments throughout the script, added functionality to write the interpreted text to a file.

--- script.py
+++ script.py
@@ -1,25 +1,41 @@
 import speech_recognition as sr
 import pyttsx3 as py
+import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+#  Initialize SpeechRecognition
 rec = sr.Recognizer()
 
+#  Opens Tkinter dialogue box to choose file
 Tk().withdraw()
 filename = askopenfilename()
+path = os.path.dirname(filename)
+print(filename, "  path:  ", path)
+textfname = filename + '.txt'
 
 
+#  Method to read string of text out loud
 def SpeakText(command):
     engine = py.init()
     engine.say(command)
     engine.runAndWait()
 
 
+#  Records audio from the given file
 file = sr.AudioFile(filename)
 with file as source:
     audio = rec.record(source)
 
 
+#  Converts the audio to text using Google
 text = rec.recognize_google(audio)
 print(text, end='\n')
 SpeakText(text)
+
+#  Writes the text to a file in the same place as the original audio
+file = open(textfname, "w+")
+file.write(text)
+file.close()
+
+
