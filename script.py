c737e6d241cf35432fe6200b0531561d896594c3
GStrongman <georgia.strongman@gmail.com>
2020-07-22 18:22:06 +1200

    Add method to interpret audio file
    Method should take in audio files and convert to text, then read the contents out.

--- script.py
+++ script.py
@@ -12,19 +12,26 @@ def SpeakText(command):
 
 SpeakText('Hi there')
 
-while 1:
-    try:
-        with sr.Microphone() as source:
-            rec.adjust_for_ambient_noise(source, duration=0.2)
-            audio = rec.listen(source)
-            text = rec.recognize_google(audio)
-            text = text.lower()
-            SpeakText(text)
+file = sr.AudioFile('Hello_There.mp3')
+with file as source:
+    audio = rec.record(source)
+    text = rec.recognize_google(audio)
+    print(text, end='\n')
+    SpeakText(text)
 
-    except sr.RequestError as error:
-        print("Error: could not request results; {0}".format(error))
-
-    except sr.UnknownValueError:
-        print("Error: unknown value occurred")
+# while 1:
+#     try:
+#         with sr.Microphone() as source:
+#             rec.adjust_for_ambient_noise(source, duration=0.2)
+#             audio = rec.listen(source)
+#             text = rec.recognize_google(audio)
+#             text = text.lower()
+#             SpeakText(text)
+#
+#     except sr.RequestError as error:
+#         print("Error: could not request results; {0}".format(error))
+#
+#     except sr.UnknownValueError:
+#         print("Error: unknown value occurred")
 
 
