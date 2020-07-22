0c11a7e0d035d69cbc37871d62135447c1ee1e8e
GStrongman <georgia.strongman@gmail.com>
2020-07-22 18:06:37 +1200

    Add method to interpret microphone
    Method should listen to the mic and interpret text - currently having trouble getting PyAudio library to work

--- script.py
+++ script.py
@@ -11,3 +11,20 @@ def SpeakText(command):
 
 
 SpeakText('Hi there')
+
+while 1:
+    try:
+        with sr.Microphone() as source:
+            rec.adjust_for_ambient_noise(source, duration=0.2)
+            audio = rec.listen(source)
+            text = rec.recognize_google(audio)
+            text = text.lower()
+            SpeakText(text)
+
+    except sr.RequestError as error:
+        print("Error: could not request results; {0}".format(error))
+
+    except sr.UnknownValueError:
+        print("Error: unknown value occurred")
+
+
