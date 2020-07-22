6be708dabe005cff7ac93be7b247df183cd6d607
GStrongman <georgia.strongman@gmail.com>
2020-07-22 18:02:22 +1200

    Add method to read text
    Method can take in a string and read the string out.

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
 import speech_recognition as sr
-import pyttsx3
+import pyttsx3 as py
 
 rec = sr.Recognizer()
 
+
+def SpeakText(command):
+    engine = py.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+SpeakText('Hi there')
